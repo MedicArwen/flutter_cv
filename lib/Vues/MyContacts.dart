5e6f893cadc0a4979bb717e002ca8263c218b4d7
MedicArwen <thierry-bru@laposte.net>
2021-04-10 22:12:36 +0200

    ajout liens clikables
    Ajout librairie pour rendre les liens clickables
    ajustement cosmétiques sur la page contact
    ajout d'un bouton vers la page web pour ajouter la vcard

--- lib/Vues/MyContacts.dart
+++ lib/Vues/MyContacts.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttercv/Models/CurriculumVitae.dart';
 import 'package:fluttercv/Models/Loisir.dart';
 import 'package:fluttercv/Vues/MyInfoGenerale/ListViews/ListViewLoisirs.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'DefautPanelView.dart';
 
@@ -41,12 +42,12 @@ class MyContactsState extends State<MyContacts> with TickerProviderStateMixin
         Stack(children: [
           Positioned(child:
           AnimatedContainer(child:Image.asset('images/photo_guitare.jpg',height:widget.height,fit: BoxFit.fitHeight),duration: Duration(seconds: 3),),top: 100-slideHeight),
+          Positioned(child: FloatingActionButton(onPressed: ()=>{_makingOpenUrl('https://opn.to/r/Carte_de_visite_de_Thierry/')},child: Icon(Icons.person_add,color: Colors.grey),backgroundColor: Colors.white24,),right:20, top:20),
           AnimatedContainer(width: widget.width,height: slideHeight,duration: Duration(milliseconds: 50),
-          //child: Row(children: [TextButton(onPressed:goUp, child: Text('UP')),TextButton(onPressed:goDown, child: Text('down'))],),
           child:GestureDetector(onPanStart: (drag){print("panstart");},onPanUpdate: (drag){print(drag.globalPosition.dy);setState(() {
             slideHeight = widget.height-drag.globalPosition.dy;
           });},child:
-            Column(children:
+           Column(children:
             [
               Center(child:Text('Pour me Contacter',style: Theme.of(widget.parentContext).textTheme.headline1,)),
               Expanded(child:ListView(children:
@@ -57,6 +58,7 @@ class MyContactsState extends State<MyContacts> with TickerProviderStateMixin
                   leading: Icon(Icons.mail_outline),
                   title: Text('thierry.bru@takotek.fr',style: Theme.of(widget.parentContext).textTheme.bodyText1),
                   tileColor: Colors.white10,
+                  onLongPress:()=>{ _makingOpenUrl('mailto:thierry.bru@takotek.fr')}
                   )),
               Card(child:
               ListTile(
@@ -64,6 +66,7 @@ class MyContactsState extends State<MyContacts> with TickerProviderStateMixin
                 leading: Icon(Icons.phone),
                 title: Text('07 63 90 59 59'),
                 tileColor: Colors.white10,
+                onLongPress:()=>{ _makingOpenUrl('tel:0033763905959')},
               )),
               Card(child:
               ListTile(
@@ -71,6 +74,8 @@ class MyContactsState extends State<MyContacts> with TickerProviderStateMixin
                 leading: Image.asset('images/logo_in.png',width: 25,height: 25,color: Colors.grey,),
                 title: Text('www.linkedin.com/in/thierry-bru/',style: Theme.of(widget.parentContext).textTheme.bodyText1),
                 tileColor: Colors.white10,
+
+                onLongPress:()=>{ _makingOpenUrl('https://www.linkedin.com/in/thierry-bru/')},
               )),
               Card(child:
               ListTile(
@@ -78,14 +83,43 @@ class MyContactsState extends State<MyContacts> with TickerProviderStateMixin
                 leading: Image.asset('images/icon_github.png',width: 25,height: 25,color: Colors.grey,),
                 title: Text('https://github.com/MedicArwen',style: Theme.of(widget.parentContext).textTheme.bodyText1),
                 tileColor: Colors.white10,
+                onLongPress: ()=>{_makingOpenUrl('https://github.com/MedicArwen')},
               )),
+                Center(child:Text('Pour partager mes données de contact...',style: Theme.of(widget.parentContext).textTheme.headline3)),
+                Card(child:ListTile(
+                  dense: true,
+                  leading: Text('1',style: Theme.of(widget.parentContext).textTheme.headline3),
+                  title: Text('Ouvrez l\application Photo sur l\autre appareil'),
+                )),
+                Card(child:ListTile(
+                  dense: true,
+                  leading: Text('2',style: Theme.of(widget.parentContext).textTheme.headline3),
+                  title: Text('Visez le QR Code en le gardant au centre du cadre'),
+                )),
+                Card(child:ListTile(
+                  dense: true,
+                  leading: Text('3',style: Theme.of(widget.parentContext).textTheme.headline3),
+                  title: Text('Appuyez sur la bulle qui vous propose d\'ouvrir le lien!'),
+                )),
+                Card(child:ListTile(
+                  dense: true,
+                  leading: Text('4',style: Theme.of(widget.parentContext).textTheme.headline3),
+                  title: Text('Acceptez l\'ajout du contact (vCard) à vos contacts !'),
+                )),
                 Image.asset('images/qr_code.png')])),
-
             ])
 
 
             ), decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(30)),
+                  boxShadow: [BoxShadow(
+                    color: Colors.black, //color of shadow
+                    spreadRadius: 5, //spread radius
+                    blurRadius: 7, // blur radius
+                    offset: Offset(5, 5), // changes position of shadow
+                    //first paramerter of offset is left-right
+                    //second parameter is top to down
+                  )],
   color: Colors.white)
           )
           ],alignment: Alignment.bottomCenter),
@@ -110,6 +144,19 @@ void goUp()
       print('nouvelle hauteur:'+slideHeight.toString());
     });
   }
+  _makingPhoneCall() async {
+    const url = 'tel:0033763905959';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Can\' call $url';
+    }
+  }
+  _makingOpenUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Can\' open $url';
+    }
+  }
 }
-//      )
-// ),
